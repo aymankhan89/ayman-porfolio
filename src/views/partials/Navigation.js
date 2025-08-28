7580d5ebc61e0aba11a69e0300fbd1e92f4d7cd7
umar mohammad sheikh <sumar@proficientnowtech.com>
2025-08-28 20:35:43 +0530

    update name

--- src/views/partials/Navigation.js
+++ src/views/partials/Navigation.js
@@ -2,7 +2,7 @@ const navigationView = () => {
   const view = `
     <div class="navigation__wrapper">
       <a class="navigation__logo nav__link word__link" href="/"
-        >Mustafa</a
+        >Syed Mustafa</a
       >
       <ul class="navigation__list">
         <li class="navigation__list__item">
